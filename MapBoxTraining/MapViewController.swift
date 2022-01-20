8631ba60927dd4c1c85785698a02c93017466212
guchan-fout <stevengc@gmail.com>
2022-01-20 15:27:58 +0900

    A demo for checking 6.4.1's download cancel crash

--- MapBoxTraining/MapViewController.swift
+++ MapBoxTraining/MapViewController.swift
@@ -6,174 +6,164 @@
 //
 
 import UIKit
-import MapboxMaps
+import Mapbox
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, MGLMapViewDelegate {
     
-    internal var mapView: MapView!
-    internal var locationManager: CLLocationManager!
-    internal var startCompassBtn: UIButton!
-    internal var stopCompassBtn: UIButton!
-    internal var home: UIButton!
-    internal let shangHaiHome = CLLocationCoordinate2D.init(latitude: 31.326055179625705, longitude: 121.45195437087595)
+    internal var downloadBtn: UIButton!
+    internal var checkDownloadBtn: UIButton!
+    internal var removeAll: UIButton!
+    
+    internal var mapView: MGLMapView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView = MapView(frame: view.bounds)
-        mapView.location.delegate = self
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-
-    }
-    
-    func initMap() {
-        print("initMap")
-        print(CLLocationManager.init().location?.coordinate ?? "no location data")
-
-        let cameraOptions = CameraOptions(center: locationManager.location?.coordinate, zoom: 10.0)
-        self.mapView.mapboxMap.setCamera(to: cameraOptions)
-        
-        /*
-        if let currentLocation = self.mapView.location.latestLocation {
-            print("latestLocation")
-            print(currentLocation.coordinate.latitude)
-            let cameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 40.7135, longitude: -74.0066), zoom: 10.0)
-            self.mapView.mapboxMap.setCamera(to: cameraOptions)
-        }
-         */
-        
+        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
+        mapView = MGLMapView(frame: view.bounds, styleURL: url)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.view.addSubview(mapView)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        view.addSubview(mapView)
         
-        mapView.location.options.puckType = .puck2D()
-        mapView.location.options.puckBearingSource = .course
+        downloadBtn = UIButton(frame: CGRect(x: 25,y: 100, width: 100,height: 30))
+        downloadBtn.setTitleColor(.blue, for: .normal)
+        downloadBtn.isHidden = false
+        downloadBtn.setTitle("download", for: .normal)
+        downloadBtn.addTarget(self, action: #selector(downloadMap), for: .touchUpInside)
+        view.addSubview(downloadBtn)
         
-        initCompassButton()
+        checkDownloadBtn = UIButton(frame: CGRect(x: 25,y: 130, width: 100,height: 30))
+        checkDownloadBtn.setTitleColor(.blue, for: .normal)
+        checkDownloadBtn.isHidden = false
+        checkDownloadBtn.setTitle("checkStatus", for: .normal)
+        checkDownloadBtn.addTarget(self, action: #selector(checkStatus), for: .touchUpInside)
+        view.addSubview(checkDownloadBtn)
+        
+        removeAll = UIButton(frame: CGRect(x: 25,y: 160, width: 100,height: 30))
+        removeAll.setTitleColor(.blue, for: .normal)
+        removeAll.isHidden = false
+        removeAll.setTitle("remove ALL", for: .normal)
+        removeAll.addTarget(self, action: #selector(removeAllMaps), for: .touchUpInside)
+        view.addSubview(removeAll)
     }
     
-    func initCompassButton() {
-        print("\(#function)")
-        startCompassBtn = UIButton(frame: CGRect(x: 5,
-                                                 y: view.bounds.height * 0.7,
-                                           width: 100,
-                                           height: 30))
-        startCompassBtn.setTitleColor(.blue, for: .normal)
-        startCompassBtn.isHidden = false
-        startCompassBtn.setTitle("Start", for: .normal)
-        startCompassBtn.addTarget(self, action: #selector(startCompass), for: .touchUpInside)
+    @objc func downloadMap() {
+        let region = MGLTilePyramidOfflineRegion(styleURL: mapView.styleURL, bounds: mapView.visibleCoordinateBounds, fromZoomLevel: mapView.zoomLevel, toZoomLevel: mapView.zoomLevel + 2)
         
-        view.addSubview(startCompassBtn)
+        setupOfflinePackHandler()
         
-        stopCompassBtn = UIButton(frame: CGRect(x: startCompassBtn.frame.origin.x,
-                                                y: startCompassBtn.frame.origin.y + startCompassBtn.bounds.height + 5,
-                                           width: 100,
-                                           height: 30))
+        // Store some data for identification purposes alongside the downloaded resources.
         
-        stopCompassBtn.setTitleColor(.blue, for: .normal)
-        stopCompassBtn.isHidden = false
-        stopCompassBtn.setTitle("Stop", for: .normal)
-        stopCompassBtn.addTarget(self, action: #selector(stopCompass), for: .touchUpInside)
-        view.addSubview(stopCompassBtn)
-        
-        home = UIButton(frame: CGRect(x: stopCompassBtn.frame.origin.x,
-                                                y: stopCompassBtn.frame.origin.y + startCompassBtn.bounds.height + 5,
-                                           width: 100,
-                                           height: 30))
-        
-        home.setTitleColor(.blue, for: .normal)
-        home.isHidden = false
-        home.setTitle("home", for: .normal)
-        home.addTarget(self, action: #selector(flyToHome), for: .touchUpInside)
-        view.addSubview(home)
-        
+        let userInfo = ["name": "\(region.bounds)"]
+        do {
+            let context = try NSKeyedArchiver.archivedData(withRootObject: userInfo, requiringSecureCoding: false)
+            
+            MGLOfflineStorage.shared.addPack(for: region, withContext: context) { (pack, error) in
+            guard error == nil else {
+                // The pack couldn’t be created for some reason.
+                print("Error: \(error?.localizedDescription ?? "unknown error")")
+                return
+            }
+            // Start downloading.
+            pack!.resume()
+            print("download start:" + region.description )}
+            } catch {
+                fatalError("Can't encode data: \(error)")
+            }
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
-        print (heading.magneticHeading)
-        let new = CameraOptions(bearing:locationManager.heading?.magneticHeading)
-        mapView.mapboxMap.setCamera(to: new)
+    func setupOfflinePackHandler() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(offlinePackProgressDidChange),
+                                               name: NSNotification.Name.MGLOfflinePackProgressChanged,
+                                               object: nil)
     }
     
-    func requstFullAccuracy(){
-        self.mapView.location.requestTemporaryFullAccuracyPermissions(withPurposeKey: "CustomKey")
-    }
-    
-    func initUI(){
-
-        // Granularly configure the location puck with a `Puck2DConfiguration`
-        let configuration = Puck2DConfiguration(topImage: UIImage(named: "star"))
-        mapView.location.options.puckType = .puck2D(configuration)
-        mapView.location.options.puckBearingSource = .course
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
         
-        // Center map over the user's current location
-        /*
-         mapView.mapboxMap.onNext(.mapLoaded, handler: { [weak self] _ in
-         guard let self = self else { return }
-         
-         if let currentLocation = self.mapView.location.latestLocation {
-         print("onNext")
-         let cameraOptions = CameraOptions(center: currentLocation.coordinate, zoom: 20.0)
-         self.mapView.camera.ease(to: cameraOptions, duration: 2.0)
-         }
-         })
+        // Create point to represent where the symbol should be placed
+        let point = MGLPointAnnotation()
+        point.coordinate = mapView.centerCoordinate
+        
+        // Create a data source to hold the point data
+        let shapeSource = MGLShapeSource(identifier: "marker-source", shape: point, options: nil)
+        
+        // Create a style layer for the symbol
+        let shapeLayer = MGLSymbolStyleLayer(identifier: "marker-style", source: shapeSource)
+        
+        // Add the image to the style's sprite
+        if let image = UIImage(named: "house-icon") {
+            style.setImage(image, forName: "home-symbol")
+        }
+        
+        // Tell the layer to use the image in the sprite
+        shapeLayer.iconImageName = NSExpression(forConstantValue: "home-symbol")
+        
+        // Add the source and style layer to the map
+        style.addSource(shapeSource)
+        style.addLayer(shapeLayer)
+    }
+    
+    @objc func checkStatus() {
+        if let packs = MGLOfflineStorage.shared.packs {
+            for pack in packs {
+                print(pack.description + pack.state.rawValue.description)
+            }
+        }
+    }
+    
+    @objc func removeAllMaps() {
+        if let packs = MGLOfflineStorage.shared.packs {
+            for pack in packs{
+                print(pack.description + pack.state.rawValue.description)
+                MGLOfflineStorage.shared.removePack(pack, withCompletionHandler: nil)
+                //if (pack.state == .complete) {}
+            }
+        }
+    }
+    
+    @objc func offlinePackProgressDidChange(notification: NSNotification) {
+        /**
+         Get the offline pack this notification is referring to,
+         along with its associated metadata.
          */
+        if let pack = notification.object as? MGLOfflinePack {
+            
+            if pack.state == .active {
+                if (pack.progress.percentCompleted > 50) {
+                    pack.suspend()
+                    print("suspend download for \(pack.description)")
+                }
+            }
+            
+            if pack.state == .inactive {
+                print("\(pack.description) is not downloading")
+            }
+            
+            if pack.state == .complete {
+                print("download completed")
+                
+                NotificationCenter.default.removeObserver(self, name: NSNotification.Name.MGLOfflinePackProgressChanged,
+                                                          object: nil)
+            }
+        }
+        // Reload the table to update the progress percentage for each offline pack.
+        //self.tableView.reloadData()
         
-        // Accuracy ring is only shown when zoom is greater than or equal to 18
-        mapView.mapboxMap.onEvery(.cameraChanged, handler: { [weak self] _ in
-            guard let self = self else { return }
-            //self.toggleAccuracyRadiusButton.isHidden = self.mapView.cameraState.zoom < 18.0
-        })
     }
-    
-
-    
-    @objc func startCompass() {
-        locationManager.startUpdatingHeading()
-    }
-    
-    @objc func stopCompass() {
-        locationManager.stopUpdatingHeading()
-    }
-    
-    @objc func flyToHome() {
-        if (self.home.title(for: .normal) == "Back") {
-            let cameraOptions = CameraOptions(center: locationManager.location?.coordinate, zoom: 10.0)
-            self.mapView.mapboxMap.setCamera(to: cameraOptions)
-            self.mapView.camera.fly(to: cameraOptions, duration: 2.0, completion: { result in
-                if (result == .end) {
-                    self.home.setTitle("Home", for: .normal)
-                }
-            })
-        } else {
-            let newCamera = CameraOptions(center: shangHaiHome)
-            self.mapView.camera.ease(to: newCamera, duration: 5.0, curve: .easeInOut, completion: { result in
-                if (result == .end) {
-                    self.home.setTitle("Back", for: .normal)
-                }
-            })
-        }
-    }
-            
 }
 
-extension MapViewController: LocationPermissionsDelegate {
-    func locationManager(_ locationManager: LocationManager, didChangeAccuracyAuthorization accuracyAuthorization: CLAccuracyAuthorization) {
-        if accuracyAuthorization == .reducedAccuracy {
-            // Perform an action in response to the new change in accuracy
-            print("reducedAccuracy")
-            //mapView.location.requestTemporaryFullAccuracyPermissions(withPurposeKey: "CustomKey")
-            
-        }
-        if accuracyAuthorization == .fullAccuracy {
-            print("fullAccuracy")
-            initMap()
-            
-        }
+fileprivate extension MGLOfflinePackProgress {
+    var percentCompleted: Float {
+        let percentage = Float(countOfResourcesCompleted) / Float(countOfResourcesExpected) * 100
+        return percentage
     }
     
+    var formattedCountOfBytesCompleted: String {
+        return ByteCountFormatter.string(fromByteCount: Int64(countOfBytesCompleted),
+                                         countStyle: .memory)
+    }
 }
-
 
 
--- Podfile
+++ Podfile
@@ -4,7 +4,8 @@
 target 'MapBoxTraining' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-  pod 'MapboxMaps', '10.1.0'
+  pod 'Mapbox-iOS-SDK'
+  #pod 'MapboxMaps', '10.1.0'
   # Pods for MapBoxTraining
 
 end
