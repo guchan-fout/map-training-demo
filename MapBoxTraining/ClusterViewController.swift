4c8d984812dd395f6fdb483ea44db09570773970
guchan-fout <stevengc@gmail.com>
2022-12-07 22:39:19 +0900

    upload a NG case for internal sharing

--- MapBoxTraining/ClusterViewController.swift
+++ MapBoxTraining/ClusterViewController.swift
@@ -50,6 +50,14 @@ class ClusterViewController: UIViewController {
         
         // here to get each point's feature, and check any of them are true, then the cluster's new parameter's value will be true
         let clusterProp: [String: Expression] = ["hasLowFlow": Exp(.any) {Exp(.lte){Exp(.get) { "FLOW" }; 100.0}}]
+        
+        /*
+        let clusterPropNG1: [String: Expression] = ["sum": Exp(.sum) { 10; 20}]
+                                                
+        let clusterPropNG2: [String: Expression] = ["sum": Exp(.sum) { 10
+                                                                       20}]
+        */
+        
         source.clusterProperties = clusterProp
         
         let sourceID = "fire-hydrant-source"
